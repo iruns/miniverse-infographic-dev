bbc434ac9105fbb4edb716cbf24df044dac217bb
irun <iruns@users.noreply.github.com>
2017-09-18 06:49:01 +0700

    trying config file

--- bin/update.js
+++ bin/update.js
@@ -1,4 +1,14 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
+var path = require("path");
 
 shell.exec("echo shell.exec works");
+console.log("working");
+
+var configPath;
+var config;
+configPath = path.resolve("miniverse.config.js");
+if(fs.existsSync(configPath)) {
+	config = require(configPath);
+    console.log(config.name);
+}
